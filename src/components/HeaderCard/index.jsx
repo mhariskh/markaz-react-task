ed1c8dea8eb0d7a71658ebf8a039003d94bfe579
mhariskh <mhkhan.bscs19seecs@seecs.edu.pk>
2023-02-14 17:47:42 +0500

    fixed button responsiveness

--- src/components/HeaderCard/index.jsx
+++ src/components/HeaderCard/index.jsx
@@ -11,11 +11,11 @@ export const HeaderCard = ({
     <div className="h-full w-full rounded-[20px] bg-white">
       <div className="flex h-full flex-col  justify-between backdrop:">
         <div className="w-full border-b  py-6 px-8 last-of-type:border-b-0 lg:px-14 grid grid-cols-12  gap-8">
-          <div className="col-span-7">
+          <div className="col-span-6">
             <h4 className="text-xl"> {title}</h4>
             <p className="font-light">{description}</p>
           </div>
-          <div className="col-span-5 ">
+          <div className="col-span-6 ">
             <button
               className="w-full border border-transparent text-sm font-medium  py-3 text-grey-700 bg-green-400 transition ease-in hover:bg-green-500 rounded-md "
               onClick={(e) => {
