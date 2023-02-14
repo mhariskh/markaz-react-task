8f226dd68ff3ae5aba858b86c084440cc509cada
mhariskh <mhkhan.bscs19seecs@seecs.edu.pk>
2023-02-14 10:11:38 +0500

    Dummy Login Page with custom styling implemented

---
+++ src/components/InputField/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const InputField = ({
+  label,
+  type,
+  placeholder,
+  error,
+  name,
+  value,
+  isDisabled,
+}) => {
+  return (
+    <>
+      <label
+        htmlFor={name}
+        className="block pb-2 text-sm font-medium text-gray-700"
+      >
+        {label}
+      </label>
+      <div className="mt-1">
+        <input
+          type={type}
+          name={name}
+          id={name}
+          autoComplete="email"
+          className="block w-full rounded-md border-gray-300 py-3 shadow-sm transition duration-200 ease-in focus:border-accent-3 focus:ring-accent-3 disabled:bg-gray-100 sm:text-sm"
+          placeholder={placeholder}
+          value={value}
+          disabled={isDisabled}
+        />
+        {error && (
+          <span className="text-md mt-2 inline-block text-red-600">
+            {error}
+          </span>
+        )}
+      </div>
+    </>
+  );
+};
+
+InputField.defaultProps = {
+  isDisabled: false,
+};
+
+export default InputField;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { mediaPaths } from "../../../src/constants";
+import { LoginForm } from "../../components";
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="h-full overflow-y-auto shadow-2xl">
+      <div className="container h-full px-6 py-12">
+        <div className="flex h-full flex-wrap items-center justify-center  gap-5 text-gray-800 lg:flex-row-reverse">
+          <div className="mb-12 md:w-8/12 lg:mb-0 lg:w-6/12">
+            <img src={mediaPaths.login} className="w-full h-20" alt="" />
+          </div>
+          <div className="md:w-8/12 lg:ml-20 lg:w-5/12 shadow-xl p-5 border rounded-md">
+            <h1 className="text-4xl font-semibold">Log In</h1>
+            <p className="my-5 text-lg font-normal text-gray-500">
+              Enter your email and password to login
+            </p>
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
